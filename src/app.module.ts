ad86544e2d7fce5131c994877308830be49bcde3
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-15 16:49:28 -0300

    Added new imports and a new provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common'
+import { APP_GUARD } from '@nestjs/core'
+import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
+
 import { UserModule } from './user/user.module'
+import { AuthModule } from './auth/auth.module'
+import { JwtAuthGuard } from './auth/jwt.auth-guard'
+import { validationSchema } from './config/validation'
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://0.0.0.0/quiz'), UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, validationSchema }),
+    MongooseModule.forRoot('mongodb://0.0.0.0/quiz'),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [{ provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
 export class AppModule {}
