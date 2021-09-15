59176bf1aa32e64a0ebaf24d82b1e825072cce05
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-15 16:48:55 -0300

    Created the authentication module with jwt

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Post } from '@nestjs/common'
+
+import { Public } from 'src/public.decorator'
+import { AuthService } from './auth.service'
+import { AuthLoginDto } from './auth.dto'
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly service: AuthService) {}
+
+  @Public()
+  @Post('login')
+  async login(@Body() authLoginDto: AuthLoginDto) {
+    return this.service.login(authLoginDto)
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { compareSync } from 'bcryptjs'
+
+import { UserService } from 'src/user/user.service'
+import { AuthLoginDto } from './auth.dto'
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser({ email, password }: AuthLoginDto) {
+    const user = await this.userService.findByEmail(email)
+
+    if (!user || !compareSync(password, user.password)) {
+      throw new UnauthorizedException('The email or password is invalid')
+    }
+
+    return user
+  }
+
+  async login(authLoginDto: AuthLoginDto) {
+    const user = await this.validateUser(authLoginDto)
+    const payload = { userId: user.id }
+
+    return { access_token: this.jwtService.sign(payload) }
+  }
+}
