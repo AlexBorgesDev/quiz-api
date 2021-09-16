4d87e768d6a2a34d74c62847f0577458f62296c9
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-16 09:26:26 -0300

    Created the tests for the auth controller

---
+++ src/auth/auth.controller.spec.ts
@@ -0,0 +1,78 @@
+import { internet } from 'faker'
+import { JwtModule } from '@nestjs/jwt'
+import { MongooseModule } from '@nestjs/mongoose'
+import { PassportModule } from '@nestjs/passport'
+import { Test, TestingModule } from '@nestjs/testing'
+import { UnauthorizedException } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+
+import { UserModule } from '../user/user.module'
+import { UserService } from '../user/user.service'
+import { JwtStrategy } from './jwt.strategy'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { validationSchema } from '../config/validation'
+
+describe('AuthController', () => {
+  let controller: AuthController
+  let userService: UserService
+
+  const userDto = { email: internet.email(), password: internet.password(10) }
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({ isGlobal: true, validationSchema }),
+        MongooseModule.forRoot('mongodb://0.0.0.0/quiz-test'),
+        UserModule,
+        PassportModule,
+        JwtModule.registerAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: async (configService: ConfigService) => ({
+            secret: configService.get<string>('JWT_SECRET'),
+            signOptions: { expiresIn: '1h' },
+          }),
+        }),
+      ],
+      controllers: [AuthController],
+      providers: [AuthService, JwtStrategy],
+    }).compile()
+
+    controller = module.get<AuthController>(AuthController)
+    userService = module.get<UserService>(UserService)
+  })
+
+  afterAll(async () => {
+    const user = await userService.findByEmail(userDto.email)
+    await userService.delete(user.id)
+  })
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined()
+  })
+
+  describe('#login', () => {
+    beforeAll(async () => {
+      await userService.create({ ...userDto, name: 'Auth Test' })
+    })
+
+    it('should not authenticate if credentials are invalid', async () => {
+      expect.assertions(3)
+      try {
+        await controller.login({ ...userDto, password: '12345678' })
+      } catch (error) {
+        error = error as UnauthorizedException
+        expect(error.name).toEqual('UnauthorizedException')
+        expect(error.response).not.toBeNull()
+        expect(error.response.statusCode).toBe(401)
+      }
+    })
+
+    it('should return access_token when credentials are valid', async () => {
+      const res = await controller.login(userDto)
+      expect(res).not.toBeNull()
+      expect(res.access_token).not.toBeNull()
+    })
+  })
+})
