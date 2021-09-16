68ee9eb82fd04c64e78e7764a15467c57500b18f
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-16 10:48:15 -0300

    Imported UserModule into QuizModule

--- src/quiz/quiz.module.ts
+++ src/quiz/quiz.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 
 import { UserSchema } from '../user/user.schema'
+import { UserModule } from '../user/user.module'
 import { QuizSchema } from './quiz.schema'
 import { QuizService } from './quiz.service'
 import { QuizController } from './quiz.controller'
@@ -12,6 +13,7 @@ import { QuizController } from './quiz.controller'
       { name: 'Quiz', schema: QuizSchema },
       { name: 'User', schema: UserSchema },
     ]),
+    UserModule,
   ],
   providers: [QuizService],
   controllers: [QuizController],
